3fadd58f55f776106372c67abe5f2a286005f9f3
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-20 19:08:13 +0100

    feat: array oggetti del team

---
+++ js/main.js
@@ -0,0 +1,53 @@
+/*
+          <!-- elemento js -->
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="-lc-img">
+              <img
+                class="img-fluid"
+                src="img/angela-caroll-chief-editor.jpg"
+                alt="Angela Carrol"
+              />
+            </div>
+            <div class="-lc-description text-center p-3 bg-light">
+              <h3>Angela Carrol</h3>
+              <div>Chief editor</div>
+            </div>
+          </div>
+          <!-- elemento js -->
+*/
+"use strict";
+//!funzioni
+
+//!programma
+const team = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    img: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    img: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    img: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    img: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    img: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    img: "barbara-ramos-graphic-designer.jpg",
+  },
+];
